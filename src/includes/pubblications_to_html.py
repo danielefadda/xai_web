28b530becca3b13c4204963b94202c486778bca2
Daniele Fadda <fadda.daniele@gmail.com>
2022-01-13 16:54:13 +0100

    new layout for publications and people completed

--- src/includes/pubblications_to_html.py
+++ src/includes/pubblications_to_html.py
@@ -1,119 +1,49 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[274]:
-
-
 import pandas as pd
 from IPython.display import HTML
 import math
 
-
-# In[275]:
-
-
+#Convert numbers to words
 import inflect
 p = inflect.engine()
 p.number_to_words(99)
 
+import urllib.request
+from urllib.error import HTTPError
+import sys
 
-# In[276]:
 
-
+# Import google spreadsheet
 sheet_id = '1VaF5jAauYYYKo1WH40kLlsQ0-azIX5eMZoFYv5JrVr0'
 sheet_names = ['papers','dataset','algorithms','masterThesis','phdThesis']
 
 
-# In[277]:
-
-
+# Function to convert text to pug file
 def write_html(string,filename='filename',extension='pug'):
     Html_file= open(f"{filename}.{extension}","w")
     Html_file.write(string)
     Html_file.close()
 
-
-# ## Papers
-
-# In[278]:
-
-
+### PAPERS
+# Import Papers
 sheet_name= sheet_names[0]
 url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
 
-
-# In[279]:
-
-
 papers = pd.read_csv(url,header=0)
 papers.columns
+# Visualization order of the papers (featured before then year of publication)
+papers=papers.sort_values(by=['featured','Year of publication'], ascending = False).reset_index(drop=True)
 
 
-# In[280]:
-
-
-bullets='<ul>'
-for i, row in papers.iterrows():
-    authors= row['Authors']
-#     pubblicationYear = int(row['Year of publication'])
-    pubYearString = row['Year of publication']
-    pubblicationYear =  '.' if pubYearString!=pubYearString else ' - '+str(int(pubYearString))+' -'
-    title=row['Title of the scientific publication'].strip('.')
-    
-    journalString=row['Title of the journal or equivalent']
-    journal='.' if journalString!=journalString else ', '+journalString.strip()+'.'
-    DOI=row['DOI']
-    
-    bullet=f"<li><strong><a href='https://doi.org/{DOI}' target='_blank'>{title}</a></strong>{pubblicationYear} {authors} {journal}</li>"
-    if row['visible on website'] == 'YES':
-        bullets+=bullet
-bullets+='</ul>'
-#display(HTML(bullets))
-
-
-# ### Pug version
-
-# In[281]:
-
-
-bullets=''
-for i, row in papers.iloc[1:].iterrows():
-    authors= row['Authors']
-#     pubblicationYear = int(row['Year of publication'])
-    pubYearString = row['Year of publication']
-    pubblicationYear =  '.' if pubYearString!=pubYearString else str(int(pubYearString))
-    
-    title=row['Title of the scientific publication'].strip('.')
-    
-    journalString=row['Title of the journal or equivalent']
-    journal='.' if journalString!=journalString else ', '+journalString.strip()+'.'
-    DOI=row['DOI']
-    
-    bullet=f'''li
-    div.mb-3
-        a(href='https://doi.org/{DOI}', target="_blank")
-            | {authors} ({pubblicationYear}). #[strong {title}]
-'''
-    if row['visible on website'] == 'YES':
-        bullets+=bullet
-# display(HTML(bullets))
-
-
-# In[282]:
-
-
-write_html(bullets, filename='paper-list')
-
-
-# In[283]:
-
+### Create Pug version for papers
 
 cards=''
 for i, row in papers.iterrows():
     authors= row['Authors']
     abstract= row['abstract']
     researchLine= row['Research line']
-    
     if isinstance(researchLine, str):
         researchLine=researchLine.replace(",", "▪")
         rlString=f'''p.my-1
@@ -121,29 +51,81 @@ for i, row in papers.iterrows():
     else:
         rlString=''
 
-    if isinstance(abstract, str):
-        abstractButton= f'''p.my-1
-            a.btn-mini.px-2.btn-secondary.small(href='#' data-toggle='collapse' data-target='#collapse-{p.number_to_words(i)}' aria-expanded='true' aria-controls='collapseAbs') Abstract'''
-    else:
-        abstractButton=''
-#     pubblicationYear = int(row['Year of publication'])
     pubYearString = row['Year of publication']
     pubblicationYear =  '.' if pubYearString!=pubYearString else str(int(pubYearString))
-    
     title=row['Title of the scientific publication'].strip('.')
-    
-    journalString=row['Title of the journal or equivalent']
-    journal='.' if journalString!=journalString else ', '+journalString.strip()+'.'
+
+    container=row['container']
+    if not isinstance(container, str):
+        container=''
+    else:
+        container=f' - {container}'
+    journal=row['Title of the journal or equivalent']
+    if not isinstance(journal, str):
+        journal=''
+    else:
+        journal=f'. In {journal}'
+
     DOI=row['DOI']
-    
+    doiLink=row['doi link']
+    image= row['image file']
+
+    if isinstance(DOI, str):
+        #Doi to bibtex
+        BASE_URL = 'http://dx.doi.org/'
+        url = BASE_URL + DOI
+        req = urllib.request.Request(url)
+        req.add_header('Accept', 'application/x-bibtex')
+        try:
+            with urllib.request.urlopen(req) as f:
+                bibtex = f.read().decode()
+                bibtex=bibtex.replace('\t','                    ')
+                bibtex=bibtex[:-1] + "                }"
+        except HTTPError as e:
+            if e.code == 404:
+                bibtex='BibTex not found'
+            else:
+                bibtex='BibTex not found.'# Service unavaillable
+    else:
+        bibtex='BibTex not found'
+
+    #Create buttons IF:
+        #More information
+    if (isinstance(abstract, str) or (bibtex not in['BibTex not found','BibTex not found.']) ):
+        if not (isinstance(abstract, str)):
+            abstract= 'ABSTRACT NOT FOUND'
+
+        abstractButton= f'''p.my-1
+            a.btn-mini.px-2.btn-secondary.small(href='#' data-toggle='collapse' data-target='#collapse-{p.number_to_words(i)}' aria-expanded='true' aria-controls='collapseAbs') More information'''
+    else:
+        abstractButton=''
+
+        #External link
+    if (isinstance(doiLink, str)):
+        externalButton= f'''p.my-1
+            a.btn-mini.px-2.btn-secondary.small(href='{doiLink}', target="_blank") External Link'''
+    else:
+        externalButton= ''
+
+    ##### string composition for pug file:
     ptOne=f'''.row.mt-4.justify-content-center
     .col-lg-1.text-right
-        h4 {i}.'''
+        h4 {i+1}.'''
+
     # summary image?
     if row['summary image']=='YES':
         ptTwo=f'''
     .col-lg-2.pl-0
-        img(src='assets/images/pubblications/pubblication_demo_img.jpg ' alt="immagine" style='width:100%;').mr-3
+        img(src='assets/images/pubblications/{image} ' alt="immagine" style='width:100%;' data-toggle='modal' data-target='#modal-{p.number_to_words(i)}' type='button').mr-3
+        #modal-{p.number_to_words(i)}.modal.fade(tabindex='-1' role='dialog' aria-labelledby='#modal-{p.number_to_words(i)}-Title' aria-hidden='true')
+            .modal-dialog.modal-lg.modal-dialog-centered(role='document')
+                .modal-content
+                    .modal-header
+                        p.small {title}
+                        button.close(type='button' data-dismiss='modal' aria-label='Close')
+                            span(aria-hidden='true') &times;
+                    .modal-body
+                        img(src='assets/images/pubblications/{image} ' alt="immagine" )
     .col-lg-6.bg-yellow.p-3'''
     else:
         ptTwo=f'''
@@ -151,143 +133,84 @@ for i, row in papers.iterrows():
 
     ptThree=f'''
         #accordion-{p.number_to_words(i)}.accordion
-        | #[strong {title}] 
+        | #[strong {title}]
         br
-        | #[em {authors}] ({pubblicationYear}) - {journalString}
+        | #[em {authors}] ({pubblicationYear}){container}{journal}
         #collapse-{p.number_to_words(i)}.collapse(aria-labelledby='heading-{p.number_to_words(i)}' data-parent='#accordion-{p.number_to_words(i)}')
             div.bg-yellow
                 hr
-                p.small ABSTRACT: {abstract}
+                p.small #[strong Abstract]
+                p.small {abstract}
+            p.small.pt-2 #[strong BibTex]
+            p.small.
+                {bibtex}
     .col-lg-2.pl-3
-        p.my-1
-            a.btn-mini.px-2.btn-secondary.small(href='https://doi.org/{DOI}', target="_blank") Resource Link
-        p.mb-1.mt-0
-            a.btn-mini.px-2.btn-secondary.small(href="about.html") BibTeX
         {abstractButton}
+        {externalButton}
         {rlString}
 '''
-    
+
     card= ptOne+ptTwo+ptThree
     if row['visible on website'] == 'YES':
         cards+=card
-    
-# display(HTML(bullets))
 
-
-# In[284]:
-
-
+### save pug file:
 write_html(cards, filename='paper-cards')
 
 
-# ## Thesis
-
-# In[135]:
-
+### THESIS
 
 sheet_name= sheet_names[3]
 url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
-
-
-# In[16]:
-
-
 masterThesis = pd.read_csv(url)
 masterThesis.columns
-
-
-# In[17]:
-
-
-bulletsThesis='<ul>'
-
-for i, row in masterThesis.iterrows():
-    author= row['Author']
-    title= row['Title']
-    title = '' if title!=title else title
-    
-    status= row['Status [Ongoing] / [completed]']
-    
-    classification = row['Thesis ']
-    year= row['Year']
-    bullet=f"<li>{author}, <strong>{title}</strong> [{classification} - {year} - {status}]</li>"
-    if row['visible on website'] == 'YES':
-        bulletsThesis+=bullet
-bulletsThesis+='</ul>'
-#display(HTML(bulletsThesis))
-
-
-# #### Pug version
-
-# In[18]:
-
 
 bulletsThesis=''
 for i, row in masterThesis.iterrows():
     author= row['Author']
     title= row['Title']
     title = '' if title!=title else title
-    
+
     status= row['Status [Ongoing] / [completed]']
-    
+
     classification = row['Thesis ']
     year= row['Year']
-    
+
     bullet=f'''li
     div.mb-3
         | {author} - #[strong {title}]. [{classification} - {year} - {status}]
 '''
     if row['visible on website'] == 'YES':
         bulletsThesis+=bullet
-    
-#     bullet=f"<li>{author}, <strong>{title}</strong> [{classification} - {year} - {status}]</li>"
-#display(HTML(bulletsThesis))
-
-
-# In[19]:
-
 
 write_html(bulletsThesis, filename='thesis-list')
 
 
-# # Algorithms made by XAI group 
-
-# In[20]:
-
-
-sheet_name= sheet_names[2]
-url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
-
-
-# In[21]:
-
-
-algo = pd.read_csv(url)
-algo.columns
-
-
-# In[22]:
-
-
-bulletsAlgo='<ul>'
-
-for i, row in algo.iterrows():
-    author= row['Authors']
-    title= row['Title']
-    title = '' if title!=title else title
-    
-    description= row['Brief Description']
-    description = '' if description!=description else ' - '+description.strip('.')+', '
-    
-    githubLink=row['github link']
-    
-    bullet=f"<li><strong>{title}</strong> {description} [ {author} ] </li>"
-    bulletsAlgo+=bullet
-bulletsAlgo+='</ul>'
-#display(HTML(bulletsAlgo))
-
-
-# In[ ]:
+# ### Algorithms made by XAI group
+#
+# sheet_name= sheet_names[2]
+# url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
+#
+#
+# algo = pd.read_csv(url)
+# algo.columns
+#
+# bulletsAlgo='<ul>'
+#
+# for i, row in algo.iterrows():
+#     author= row['Authors']
+#     title= row['Title']
+#     title = '' if title!=title else title
+#
+#     description= row['Brief Description']
+#     description = '' if description!=description else ' - '+description.strip('.')+', '
+#
+#     githubLink=row['github link']
+#
+#     bullet=f"<li><strong>{title}</strong> {description} [ {author} ] </li>"
+#     bulletsAlgo+=bullet
+# bulletsAlgo+='</ul>'
+# display(HTML(bulletsAlgo))
 
 
 
